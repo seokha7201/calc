d15a531ade7a4a48d9f824a9cec336b375a493dd
Yeong-Seok Lee <seokha7201@gmail.com>
2025-11-30 01:55:31 +0900

    Initial commit

---
+++ build/CMakeFiles/arithmetic.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arithmetic.dir/add.c.o"
+  "CMakeFiles/arithmetic.dir/add.c.o.d"
+  "CMakeFiles/arithmetic.dir/dur.c.o"
+  "CMakeFiles/arithmetic.dir/dur.c.o.d"
+  "CMakeFiles/arithmetic.dir/mul.c.o"
+  "CMakeFiles/arithmetic.dir/mul.c.o.d"
+  "CMakeFiles/arithmetic.dir/sub.c.o"
+  "CMakeFiles/arithmetic.dir/sub.c.o.d"
+  "libarithmetic.pdb"
+  "libarithmetic.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arithmetic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
